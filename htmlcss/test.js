547666e587c78f4f22e914277f8c8a77793ab179
an0n <an0n@localhost.localdomain>
2018-11-09 09:28:17 +0530

    homework

---
+++ htmlcss/test.js
@@ -0,0 +1,18 @@
+function read() {
+		console.log("working4");
+		
+		
+		window.alert("working2");
+		
+		var v =document.getElementById("roll").value;
+		var vv =document.getElementById("addmission").value;
+		console.log(v);
+		console.log(vv);
+		alert(vv);
+		//if((document.getElementById("roll").value)<10){
+		//	alert("work")
+		//}
+			
+		//console.log(v);
+		//}
+	}
