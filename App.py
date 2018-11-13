f7dd04ed45d2b3e7163580011e807570f918e595
ubuntu <sreevinak1@gmail.com>
2018-11-13 12:22:46 +0530

    fifth day

---
+++ App.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+app=Flask(__name__)
+@app.route('/')
+
+def index():
+    return 'Welcome to flask'
+
+@app.route('/about')
+def about():
+    return 'Welcome to ABOUT page'
+
+@app.route('/home')
+def home():
+    return 'Welcome to HOME page'
+
+if(__name__=='__main__'):
+    app.run(debug=True)
+
+
